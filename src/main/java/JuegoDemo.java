8e8bbf6e0a2a67b4bf0aed7c32962a7e61084ee7
Jorge Ernesto Nicho Galagarza <40152300+THEFLILUX@users.noreply.github.com>
2020-12-23 12:34:19 -0500

    Update JuegoDemo.java

--- src/main/java/JuegoDemo.java
+++ src/main/java/JuegoDemo.java
@@ -13,7 +13,14 @@ public class JuegoDemo {
         String opcion = myObj.nextLine();
 
         if(opcion == "1"){
-            juegoFacade.elegirPos(1,3,'a');
+            juegoFacade.elegirPos(0,0,'a');
+            juegoFacade.elegirPos(1,0,'b');
+            juegoFacade.elegirPos(2,1,'c');
+            juegoFacade.elegirPos(3,1,'d');
+            juegoFacade.elegirPos(4,0,'e');
+            juegoFacade.elegirPos(5,0,'f');
+            juegoFacade.elegirPos(6,1,'g');
+            juegoFacade.elegirPos(5,1,'h');
         } else{
             logger.info("Vuelve pronto!");
             System.exit(1);
