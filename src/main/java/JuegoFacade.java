58609fc0119e3684344e47707c04da5d9a77d868
Jorge Ernesto Nicho Galagarza <40152300+THEFLILUX@users.noreply.github.com>
2020-12-23 12:33:26 -0500

    Update JuegoFacade.java

--- src/main/java/JuegoFacade.java
+++ src/main/java/JuegoFacade.java
@@ -13,53 +13,54 @@ public class JuegoFacade {
 
     public void elegirPos(int x, int y, char figura){
         if(figura == 'a'){
-            for (int i = x; i<9; i++){
-                for (int j = y; j<9; j++){
-                    tablero.table[i][j]=pieza.pieza_a()[i][j];
+            for (int i = x; i<x+3; i++){
+                for (int j = y; j<y+3; j++){
+                    tablero.table[i][j]=pieza.pieza_a()[i-x][j-y];
                 }
             }
         } else if(figura == 'b'){
-            for (int i = x; i<9; i++){
-                for (int j = y; j<9; j++){
-                    tablero.table[i][j]=pieza.pieza_b()[i][j];
+            for (int i = x; i<x+3; i++){
+                for (int j = y; j<y+3; j++){
+                    tablero.table[i][j]=pieza.pieza_b()[i-x][j-y];
                 }
             }
         } else if(figura == 'c'){
-            for (int i = x; i<9; i++){
-                for (int j = y; j<9; j++){
-                    tablero.table[i][j]=pieza.pieza_c()[i][j];
+            for (int i = x; i<x+3; i++){
+                for (int j = y; j<y+3; j++){
+                    tablero.table[i][j]=pieza.pieza_c()[i-x][j-y];
                 }
             }
         } else if(figura == 'd'){
-            for (int i = x; i<9; i++){
-                for (int j = y; j<9; j++){
-                    tablero.table[i][j]=pieza.pieza_d()[i][j];
+            for (int i = x; i<x+3; i++){
+                for (int j = y; j<y+3; j++){
+                    tablero.table[i][j]=pieza.pieza_d()[i-x][j-y];
                 }
             }
         } else if(figura == 'e'){
-            for (int i = x; i<9; i++){
-                for (int j = y; j<9; j++){
-                    tablero.table[i][j]=pieza.pieza_e()[i][j];
+            for (int i = x; i<x+3; i++){
+                for (int j = y; j<y+3; j++){
+                    tablero.table[i][j]=pieza.pieza_e()[i-x][j-y];
                 }
             }
         } else if(figura == 'f'){
-            for (int i = x; i<9; i++){
-                for (int j = y; j<9; j++){
-                    tablero.table[i][j]=pieza.pieza_f()[i][j];
+            for (int i = x; i<x+3; i++){
+                for (int j = y; j<y+3; j++){
+                    tablero.table[i][j]=pieza.pieza_f()[i-x][j-y];
                 }
             }
         } else if(figura == 'g'){
-            for (int i = x; i<9; i++){
-                for (int j = y; j<9; j++){
-                    tablero.table[i][j]=pieza.pieza_g()[i][j];
+            for (int i = x; i<x+3; i++){
+                for (int j = y; j<y+3; j++){
+                    tablero.table[i][j]=pieza.pieza_g()[i-x][j-y];
                 }
             }
         } else if(figura == 'h'){
-            for (int i = x; i<9; i++){
-                for (int j = y; j<9; j++){
-                    tablero.table[i][j]=pieza.pieza_h()[i][j];
+            for (int i = x; i<x+3; i++){
+                for (int j = y; j<y+3; j++){
+                    tablero.table[i][j]=pieza.pieza_h()[i-x][j-y];
                 }
             }
         }
     }
 }
+
