ae0982715364c5a84685509ff133816889f9f03f
Jorge Ernesto Nicho Galagarza <40152300+THEFLILUX@users.noreply.github.com>
2020-12-23 12:35:44 -0500

    Update TestFacadePatternShapes.java

--- src/test/java/TestFacadePatternShapes.java
+++ src/test/java/TestFacadePatternShapes.java
@@ -7,13 +7,20 @@ public class TestFacadePatternShapes {
     public void Test0(){
         JuegoFacade juegoFacade = new JuegoFacade();
 
-        //juegoFacade.elegirPos(1,3,'a');
+        juegoFacade.elegirPos(0,0,'a');
+        juegoFacade.elegirPos(1,0,'b');
+        juegoFacade.elegirPos(2,1,'c');
+        juegoFacade.elegirPos(3,1,'d');
+        juegoFacade.elegirPos(4,0,'e');
+        juegoFacade.elegirPos(5,0,'f');
+        juegoFacade.elegirPos(6,1,'g');
+        juegoFacade.elegirPos(5,1,'h');
     }
 
     @Test(invocationCount = 80, threadPoolSize = 80)
     public void testTime() throws Exception {
         long startTime = System.currentTimeMillis();
-        //generic(0);
+        Test0();
         long endTime = System.currentTimeMillis();
         long time = endTime - startTime;
         Assert.assertTrue(time <= 2000);
